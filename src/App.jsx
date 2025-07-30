9f262f4a96afa2236ec0bb56e0cc4889b3942db0
Neha Yadav <ny663922@gmail.com>
2025-07-31 02:53:14 +0530

    added update butoon

--- src/App.jsx
+++ src/App.jsx
@@ -74,6 +74,13 @@ function App() {
 
         {/* Left side - Form */}
         <div className="bg-white p-6 rounded shadow">
+          {/* Progress Bar */}
+          <div className="w-full bg-gray-200 rounded-full h-2 mb-6">
+            <div
+              className="bg-blue-500 h-2 rounded-full transition-all duration-300"
+              style={{ width: `${(step / 6) * 100}%` }}
+            ></div>
+          </div>
           {renderStepForm()}
 
           <div className="flex justify-between mt-6">
